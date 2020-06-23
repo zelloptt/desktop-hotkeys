ad312085526edfe81b07039006ddfd6943a240b6
EugeneP-Zello <jalp@zello.com>
2020-06-23 05:02:51 +0300

    changes per cr

--- binding.gyp
+++ binding.gyp
@@ -1,12 +1,13 @@
 
 {
     "targets": [{
-        "target_name": "desktophotkeys",
+        "target_name": "desktop_hotkeys",
         "cflags!": [ "-fno-exceptions" ],
         "cflags_cc!": [ "-fno-exceptions" ],
         "sources": [
             "src/main.cpp",
-            "src/Hotkeys.cpp"
+            "src/Hotkeys.cpp",
+			"src/HotkeyManager.cpp",
         ],
         'include_dirs': [
             "<!@(node -p \"require('node-addon-api').include\")"
---
+++ src/HotkeyManager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <map>
+
+class HotKeyManager
+{
+	HANDLE _hThread;
+	HANDLE _hStartEvent;
+	unsigned _uThreadId;
+	HWND _hWnd;
+	typedef std::map<unsigned, std::pair<Napi::ThreadSafeFunction, Napi::ThreadSafeFunction>> TCONT;
+	TCONT _hotkeys;
+public:
+	static unsigned __stdcall winThread(void* ptr);
+	HotKeyManager();
+	~HotKeyManager();
+	bool Valid() const;
+	void NotifyHotKeyEvent(unsigned uCode, bool bPressed);
+	void UpdateCallbacks(unsigned uCode, bool bSetInUse);
+	DWORD registerShortcut(WORD wKeyCode, WORD wMod, const Napi::ThreadSafeFunction& tsfPress, const Napi::ThreadSafeFunction& tsfRelease);
+	DWORD unregisterShortcut(DWORD dwId);
+	DWORD unregisterAllShortcuts();
+};
